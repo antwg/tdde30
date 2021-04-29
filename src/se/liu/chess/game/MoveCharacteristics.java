cab379cc44120ce9793350ca0d771326dd1d9ffc
Benjamin Sundvall <bensu844@student.liu.se>
2021-04-29 14:07:34 +0200

    Added comments to MoveCharacteristics

--- src/se/liu/chess/game/MoveCharacteristics.java
+++ src/se/liu/chess/game/MoveCharacteristics.java
@@ -2,5 +2,9 @@ package se.liu.chess.game;
 
 public enum MoveCharacteristics
 {
-    HARMLESS, DOUBLESTEP, CASTLING, PROMOTING, ENPASSANT
+    HARMLESS,       // Moves that can not capture a piece, for example castling and pawn moves forward.
+    DOUBLESTEP,     // The double step a pawn can make. This allows an en passant to happen on the next turn.
+    CASTLING,       // Castling. This type of move requires a rook to be moved along with the king.
+    PROMOTING,      // Promotion. This type of move transforms a piece into another piece.
+    ENPASSANT       // En passant. This captures a piece on a square different from the target square.
 }
